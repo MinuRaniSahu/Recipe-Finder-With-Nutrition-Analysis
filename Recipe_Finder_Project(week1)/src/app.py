bcdfef9e453c23dad1c684e77b07b6a8f1f8deef
MinuRaniSahu <minuranisahu712@gmail.com>
2026-02-28 14:17:34 +0530

    Update app.py

--- Recipe_Finder_Project(week1)/src/app.py
+++ Recipe_Finder_Project(week1)/src/app.py
@@ -6,14 +6,15 @@ st.title("Recipe Finder")
 dish = st.text_input("Enter recipe name")
 
 if dish:
-    result = DevSearch_expedition(dish)
-    st.write(result)
-if isinstance(result, dict):
-    st.subheader("Ingredients")
-    for item in result["ingredients"]:
-     st.write("- " + item)
+    recipe = DevSearch_expedition(dish)
 
-    st.subheader("Instructions")
-    st.write(result["instructions"])
-else:
-     st.write(result)
+    if recipe:
+        st.subheader("Ingredients:")
+        for item in recipe["ingredients"]:
+            st.write("- " + item)
+
+        st.subheader("Steps:")
+        for step in recipe["steps"]:
+            st.write("- " + step)
+    else:
+        st.write("Recipe not found.")
