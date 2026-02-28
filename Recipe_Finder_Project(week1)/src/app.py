6db2f74d17001670733f701263c453ba10eba93f
MinuRaniSahu <minuranisahu712@gmail.com>
2026-02-28 19:45:27 +0530

    Update app.py

--- Recipe_Finder_Project(week1)/src/app.py
+++ Recipe_Finder_Project(week1)/src/app.py
@@ -1,8 +1,7 @@
 import requests
 
 # Replace this with your Spoonacular API key
-API_KEY = "PASTE_YOUR_KEY_HERE"
-
+API_KEY = 77dec99f1d134a65899d295ef2386615"
 def DevSearch_expedition(dish):
     """
     Searches for a recipe by name using Spoonacular API
