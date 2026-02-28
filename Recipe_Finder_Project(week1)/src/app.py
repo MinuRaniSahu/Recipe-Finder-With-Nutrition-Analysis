d7bfe558ccde9ca51cf45a11b23a0dc7ae13e206
MinuRaniSahu <minuranisahu712@gmail.com>
2026-02-28 13:57:01 +0530

    Update app.py

--- Recipe_Finder_Project(week1)/src/app.py
+++ Recipe_Finder_Project(week1)/src/app.py
@@ -1,20 +1,10 @@
 import streamlit as st
-from main1 import search_recipe
-from main2 import get_nutrition
-from main3 import week3_function
-from main4 import week4_function
+from main1 import DevSearch_expedition
 
-st.title("Recipe Finder with Nutrition Analysis")
+st.title("Recipe Finder")
 
-recipe_input = st.text_input("Enter recipe name or ingredient:")
+dish = st.text_input("Enter recipe name")
 
-if recipe_input:
-    try:
-        st.write(search_recipe(recipe_input))
-        st.write(get_nutrition(recipe_input))
-        st.write(week3_function(recipe_input))
-        st.write(week4_function(recipe_input))
-    except Exception as e:
-        st.error(f"Error: {e}")
-else:
-    st.info("Please enter a recipe name or ingredient")
+if dish:
+    result = DevSearch_expedition(dish)
+    st.write(result)
